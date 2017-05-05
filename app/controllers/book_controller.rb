d41f22b3b481af8dfb6743146223ed55e01a82d8
github username <github email address>
2017-05-04 20:40:56 -0400

    books-new action

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,6 +1,26 @@
 class BookController < ApplicationController
 	get '/books' do 
 		@books = Book.all 
-		erb :"/books/index"
+		erb :"books/index"
+	end
+
+	get '/books/new' do 
+		erb :"books/new"
+	end
+
+	post '/books' do 
+		if !params["topic"]["name"].empty?
+      		@book.topics << Topic.create(name: params["topic"]["name"])
+  		end
+  		
+  		@book = Book.create(params[:book])
+  		@book.save
+
+  		redirect to "books/#{@book.id}"
+
+	end
+
+	get '/books/:id' do 
+		erb :'books/show'
 	end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
-user = User.create(username: "example", email: "example@gmail.com", password_digest: "1234")
-ruby = Topic.create(name: "Ruby")
-js = Topic.create(name: "JavaScript")
-Book.create(title: "Complete Series", format: "ebook", note: "didn't start yet", topic: ruby, user: user)
-Book.create(title: "Beginner", format: "hard copy", note: "finished", topic: js, user: user)
+Topic.create(name: "Ruby/Rails")
+Topic.create(name: "JavaScript")
+Topic.create(name: "General")
+Topic.create(name: "Python")
+Topic.create(name: "HTML/CSS")
+
+
